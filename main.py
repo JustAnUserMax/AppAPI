983f9343f6ddb944e081c4133f765f86a4d2e578
Maxx <6ttahust@gmail.com>
2022-09-18 22:17:12 +0300

    Регулярное выражение

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import os.path
+import re
 import requests
 from sys import platform
 
@@ -45,6 +46,13 @@ today = datetime.datetime.today()
 # Создание папки tasks
 if not os.path.isdir('./tasks'):
     os.mkdir('./tasks')
+# Шаблон для регулярного выражения
+regex = re.compile(r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=^_`{|}~-]+)*
+|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]
+|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?
+|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])
+|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]
+|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
 # Выборка задач для отчёта и запись в файл
 for user in users:
     # Указана ли компания
@@ -54,6 +62,10 @@ for user in users:
     # Указан ли email
     if user.get('email') is None:
         print(f'У пользователя {user.get("username")} не указана почта.')
+        break
+    # Валидация почты
+    elif re.fullmatch(regex, user.get('email')):
+        print(f'У пользователя {user.get("username")} неправильно указана почта.')
         break
     # Выборка задач для отчёта
     true_todos = ''
