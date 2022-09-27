8a69eb332cb89ccab4b577dd21b197e3b8c8819a
Maxx <6ttahust@gmail.com>
2022-09-28 02:05:58 +0300

    Finished

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from sys import platform
 
 # Класс для записи пользователей
 class User(object):
-    def __init__(self, _id, name, username, email, company_name):
+    def __init__(self, _id, name, username, email, company_name, true_todos, false_todos, num_of_true_todos,
+                 num_of_false_todos):
         self.id = _id
         self.name = name
         self.username = username
         self.email = email
         self.company_name = company_name
+        self.true_todos = true_todos
+        self.false_todos = false_todos
+        self.num_of_true_todos = num_of_true_todos
+        self.num_of_false_todos = num_of_false_todos
 
 
 # Класс для записи задач
@@ -59,7 +64,7 @@ class JsonLoaderInClass:
 
     # Загрузка из json в User
     @staticmethod
-    def loader_to_User(response):
+    def loader_to_User(response, true_todos, false_todos, num_of_true_todos, num_of_false_todos):
         # Проверка валидности почты
         if Validator.is_valid_email(response.get('email')):
             return User(
@@ -67,7 +72,11 @@ class JsonLoaderInClass:
                 response.get('name'),
                 response.get('username'),
                 response.get('email'),
-                response.get('company').get('name')
+                response.get('company').get('name'),
+                true_todos,
+                false_todos,
+                num_of_true_todos,
+                num_of_false_todos
             )
         else:
             return Exception
@@ -77,7 +86,7 @@ class JsonLoaderInClass:
     def loader_to_Todo(response):
         title = ''
         # Обрезка строки до 48 символов
-        if len(response.get('title')) > 48:
+        if response.get('title') is not None and len(response.get('title')) > 48:
             title = f'{response.get("title")[:48]}...'
         else:
             title = response.get('title')
@@ -94,6 +103,12 @@ today = datetime.datetime.today()
 
 
 # Запись файла
+def write_file(path, temp):
+    f = open(path, 'w+')
+    f.write(temp)
+    f.close()
+
+
 def write_file_with_template(username, temp):
     path = f'./tasks/{username}.txt'
     path_to_old_file_for_linux = f'./tasks/old_{username}_{today.strftime("%Y-%m-%d")}T' \
@@ -108,31 +123,21 @@ def write_file_with_template(username, temp):
             if os.path.isfile(path_to_old_file_for_linux):
                 os.rename(path, f'./tasks/old_{username}_{today.strftime("%Y-%m-%d")}T'
                                 f'{today.strftime("%H:%M:%S")}.txt')
-                f = open(path, 'w+')
-                f.write(temp)
-                f.close()
+                write_file(path, temp)
             else:
                 os.rename(path, path_to_old_file_for_linux)
-                f = open(path, 'w+')
-                f.write(temp)
-                f.close()
+                write_file(path, temp)
         else:
             # Проверка на то, есть ли старый отчёт (возможно использование скрипта в ту же минуту)
             if os.path.isfile(path_to_old_file_for_windows):
                 os.rename(path, f'./tasks/old_{username}_{today.strftime("%Y-%m-%d")}T'
                                 f'{today.strftime("%H.%M.%S")}.txt')
-                f = open(path, 'w+')
-                f.write(temp)
-                f.close()
+                write_file(path, temp)
             else:
                 os.rename(path, path_to_old_file_for_windows)
-                f = open(path, 'w+')
-                f.write(temp)
-                f.close()
+                write_file(path, temp)
     else:
-        f = open(path, 'w+')
-        f.write(temp)
-        f.close()
+        write_file(path, temp)
 
 
 def main():
@@ -146,50 +151,63 @@ def main():
     response_todos = Connector.connection(todos)
     # Словарь для users и todos и списки для хранения данных
     users_match_with_todos = {}
-    class_of_users = []
+    list_of_todos = []
+    for todo in response_todos:
+        list_of_todos.append(JsonLoaderInClass.loader_to_Todo(todo))
+    list_of_users = []
     # Соотнесение пользователя и его задач
     for user in response_users:
-        class_of_users.append(JsonLoaderInClass.loader_to_User(user))
-        todos_of_the_one_user = []
-        for todo in response_todos:
-            if todo.get('userId') == user.get('id'):
-                todos_of_the_one_user.append(JsonLoaderInClass.loader_to_Todo(todo))
-        users_match_with_todos[f'{JsonLoaderInClass.loader_to_User(user).id}'] = todos_of_the_one_user
-    # Запись отчётов
-    for u in class_of_users:
-        if users_match_with_todos.get(f'{u.id}') is None:
-            print(f'У пользователя {u.username} нет задач.')
+        todos_of_the_one_user = list(filter(lambda item: item.user_id == user.get('id'), list_of_todos))
+        # Контроль filter
+        if todos_of_the_one_user[0].user_id != user.get('id'):
             break
-        # Поиск завершенных и оставшихся задач
-        true_todo_in_main = ''
-        false_todo_in_main = ''
-        num_of_true_todos = 0
-        num_of_false_todos = 0
-        for t in users_match_with_todos.get(f'{u.id}'):
-            if t.completed:
-                true_todo_in_main += f'\n{t.title}'
-                num_of_true_todos += 1
-            else:
-                false_todo_in_main += f'\n{t.title}'
-                num_of_false_todos += 1
+        true_todos = list(filter(lambda todo: todo.completed is True, todos_of_the_one_user))
+        false_todos = list(filter(lambda todo: todo.completed is False, todos_of_the_one_user))
+        # Проверка на то, какие конкретно существуют задачи
+        if true_todos[0].completed is True:
+            true_todos = list(map(lambda todo: todo.title, true_todos))
+            num_of_true_todos = len(true_todos)
+            true_todos = str(true_todos).strip('[]').replace(', ', '\n').replace("'", '')
+        else:
+            true_todos = None
+            num_of_true_todos = 0
+        if false_todos[0].completed is False:
+            false_todos = list(map(lambda todo: todo.title, false_todos))
+            num_of_false_todos = len(false_todos)
+            false_todos = str(false_todos).strip('[]').replace(', ', '\n').replace("'", '')
+        else:
+            false_todos = None
+            num_of_false_todos = 0
+        list_of_users.append(JsonLoaderInClass.loader_to_User(user,
+                                                              true_todos,
+                                                              false_todos,
+                                                              num_of_true_todos,
+                                                              num_of_false_todos
+                                                              ))
+        users_match_with_todos[f'{list_of_users[-1].id}'] = todos_of_the_one_user
+    # Запись отчетов
+    for user in list_of_users:
+        if users_match_with_todos.get(f'{user.id}') is None:
+            print(f'У пользователя {user.username} нет задач.')
+            break
         # Шаблон для записи
-        template = f'Отчёт для {u.company_name}.\n{u.name} <{u.email}> ' \
+        template = f'Отчёт для {user.company_name}.\n{user.name} <{user.email}> ' \
                    f'{today.strftime("%Y.%m.%d")} {today.strftime("%H:%M")}\nВсего задач: ' \
-                   f'{num_of_true_todos + num_of_false_todos}\n\n' \
-                   f'Завершённые задачи ({num_of_true_todos}):{true_todo_in_main}\n\nОставшиеся задачи (' \
-                   f'{num_of_false_todos}):{false_todo_in_main}'
-        if true_todo_in_main == '':
-            template = f'Отчёт для {u.company_name}.\n{u.name} <{u.email}> ' \
+                   f'{user.num_of_true_todos + user.num_of_false_todos}\n\n' \
+                   f'Завершённые задачи ({user.num_of_true_todos}):\n{user.true_todos}\n\nОставшиеся задачи (' \
+                   f'{user.num_of_false_todos}):\n{user.false_todos}'
+        if user.true_todos is None:
+            template = f'Отчёт для {user.company_name}.\n{user.name} <{user.email}> ' \
                        f'{today.strftime("%Y.%m.%d")} {today.strftime("%H:%M")}\nВсего задач: ' \
-                       f'{num_of_false_todos}\n\n' \
+                       f'{user.num_of_false_todos}\n\n' \
                        f'Завершённых задач нет.\n\nОставшиеся задачи (' \
-                       f'{num_of_false_todos}):{false_todo_in_main}'
-        if false_todo_in_main == '':
-            template = f'Отчёт для {u.company_name}.\n{u.name} <{u.email}> ' \
+                       f'{user.num_of_false_todos}):\n{user.false_todos}'
+        if user.false_todos is None:
+            template = f'Отчёт для {user.company_name}.\n{user.name} <{user.email}> ' \
                        f'{today.strftime("%Y.%m.%d")} {today.strftime("%H:%M")}\nВсего задач: ' \
-                       f'{num_of_true_todos}\n\n' \
-                       f'Завершённые задачи ({num_of_true_todos}):{true_todo_in_main}\n\nОставшихся задач нет.'
-        write_file_with_template(u.username, template)
+                       f'{user.num_of_true_todos}\n\n' \
+                       f'Завершённые задачи ({user.num_of_true_todos}):\n{user.true_todos}\n\nОставшихся задач нет.'
+        write_file_with_template(user.username, template)
 
 
 if __name__ == '__main__':
